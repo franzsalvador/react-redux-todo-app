ed0dd8bc5472b4c70d30cd95005578a56b413710
FranzCSUF <fsalvador79@gmail.com>
2018-11-01 23:00:01 -0700

    Removing types.js file.

--- client/actions/todo-actions.js
+++ client/actions/todo-actions.js
@@ -1,21 +1,19 @@
-import { GET_TODOS, ADD_TODO, COMPLETE_TODO } from './types'
-
 export const getToDos = () => {
   return {
-    type: GET_TODOS
+    type: 'GET_TODOS'
   }
 }
 
 export const addToDo = toDo => {
   return {
-    type: ADD_TODO,
+    type: 'ADD_TODO',
     toDo
   }
 }
 
 export const completeToDo = id => {
   return {
-    type: COMPLETE_TODO,
+    type: 'COMPLETE_TODO',
     id
   }
 }
