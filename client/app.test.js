6e8db06a28d886522dde607e9e36fd491c222494
FranzCSUF <fsalvador79@gmail.com>
2018-11-09 23:15:12 -0800

    Completed preliminary tests for app.js.

--- client/app.test.js
+++ client/app.test.js
@@ -1,38 +1,34 @@
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
-import ToDoList from './app'
-import configureMockStore from 'redux-mock-store'
+import configureStore from 'redux-mock-store'
+import PropTypes from 'prop-types'
+import checkPropTypes from 'check-prop-types'
 
-const mockStore = configureMockStore()
-const store = mockStore({})
+import ToDoList from './app'
+
+const mockStore = configureStore()
+
+const toDos = [
+  {
+    id: 0,
+    item: 'Walk the dog.',
+    isDone: false
+  },
+  {
+    id: 1,
+    item: 'Paint the fence.',
+    isDone: false
+  }
+]
+
+const store = mockStore(toDos)
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
-const setup = (props = {}, state = null) => {
-  return shallow(<ToDoList store={store} {...props}/>)
-}
-const findByTestAttr = (wrapper, val) => {
-  return wrapper.find(`[data-test='${val}']`)
-}
-it('renders without error', () => {
-  // const wrapper = setup()
-  // const appComponent = findByTestAttr(wrapper, 'component-app')
-  // expect(appComponent.length).toBe(1)
-})
-
-it('when submit is clicked, the todo is added', () => {
-
-})
-
-it('when a todo is clicked, the todo toggled for being done', () => {
-
-})
-
-it('when todos are added, status message is updated', () => {
-
-})
-
-it('when todo is clicked, status message is updated', () => {
-
+describe('<ToDoList/>', () => {
+  it('renders without error', () => {
+    const wrapper = shallow(<ToDoList store={store} />)
+    expect(wrapper).toMatchSnapshot()
+  })
 })
